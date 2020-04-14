e96c16407da501dc0705e33c725821abca642113
Ole Martin Ruud <dev@barskern.no>
2020-04-14 04:28:49 +0200

    Change hcl conversion function

--- lib/puppet/functions/nomad/loadhcl.rb
+++ lib/puppet/functions/nomad/loadhcl.rb
@@ -3,6 +3,6 @@ require 'json'
 Puppet::Functions.create_function(:'nomad::loadhcl') do
   # This function takes a absolute path to a HCL file and loads it to equivalent Hash.
   def loadhcl(path)
-    JSON.parse(`nomad job run -output #{path}`)
+    JSON.parse(`nomad run -output #{path}`)
   end
 end
