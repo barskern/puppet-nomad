e6306b366e14c2b20ad67cfa90d96db9c4d37afe
Ole Martin Ruud <dev@barskern.no>
2020-04-27 13:30:15 +0200

    Write hcl content to temporary file

--- lib/puppet/functions/nomad/loadhcl.rb
+++ lib/puppet/functions/nomad/loadhcl.rb
@@ -1,8 +1,16 @@
 require 'json'
+require 'tempfile'
 
 Puppet::Functions.create_function(:'nomad::loadhcl') do
   # This function takes a absolute path to a HCL file and loads it to equivalent Hash.
-  def loadhcl(path)
-    JSON.parse(`nomad run -output #{path}`)
+  def loadhcl(hcl)
+    f = Tempfile.new('nomad_loadhcl')
+    begin
+      f.write hcl
+      f.close
+      JSON.parse(`nomad run -output #{f.path}`)
+    ensure
+      f.close!
+    end
   end
 end
